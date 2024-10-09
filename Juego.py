069c4aab7a3a8aad0c8409eb02beef91e7b6cb1d
WashOrt26 <michaelw.ortegah@unac.edu.co>
2024-10-09 13:54:14 -0500

    Introducción de imagenes y empezando el fondo del juego.

--- Juego.py
+++ Juego.py
@@ -5,6 +5,19 @@ pygame.init()
 
 screen_width = 500
 screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#Cargando imagenes
+fondo2 = pygame.image.load('imagenes/Fondo2.png')
+
+snoopy_caminando_1 = pygame.image.load('imagenes/Sprites_snoopy/Snoopy_caminando_1.png')
+snoopy_caminando_2 = pygame.image.load('imagenes/Sprites_snoopy/Snoopy_caminando_2.png')
+snoopy_caminando_13 = pygame.image.load('imagenes/Sprites_snoopy/Snoopy_caminando_3.png')
+
+
+#Fondos
+screen.blit(fondo2, (0,0))
+pygame.display.flip()
 
 screen = pygame.display.set_mode((screen_height, screen_width))
 pygame.display.set_caption('plataformer')
@@ -17,3 +30,4 @@ while run:
             run = False
 
 
+pygame.quit()
